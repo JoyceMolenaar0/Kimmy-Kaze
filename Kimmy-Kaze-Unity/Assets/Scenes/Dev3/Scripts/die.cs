4dd17e341e0d2826eaf279f0ddd9a26aa2b394e9
SvenVanDerMeij <38007@ma-web.nl>
2024-09-05 15:46:29 +0200

    helkp ehlp halles gaat foustsfoutgoed

--- Kimmy-Kaze-Unity/Assets/Scenes/Dev3/Scripts/die.cs
+++ Kimmy-Kaze-Unity/Assets/Scenes/Dev3/Scripts/die.cs
@@ -1,15 +1,28 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class die : MonoBehaviour
 {
+
+    Scene sceneLoaded = SceneManager.GetActiveScene();
+    // loads next level
+    
+    public int sceneID = SceneManager.GetActiveScene().buildIndex;
     public Transform target;
     public float proximityThreshold = 5f;
+    public int nextScene = 0;
 
+    private void Start()
+    {
+        nextScene = sceneID + 1;
+    }
     void Update()
     {
         DetectProximity();
+        
+        Debug.Log(nextScene);
     }
 
     void DetectProximity()
@@ -18,9 +31,11 @@ public class die : MonoBehaviour
         {
             float distance = Vector3.Distance(transform.position, target.position);
 
-            if ((distance <= proximityThreshold) && (Input.GetKeyDown(KeyCode.Space)))
+            if ((distance <= proximityThreshold) && (Input.GetKeyDown(KeyCode.Q)))
             {
                 Debug.Log("Explode");
+                SceneManager.LoadScene(sceneLoaded.buildIndex + 1);
+
             }
             else if (distance <= proximityThreshold)
             {
